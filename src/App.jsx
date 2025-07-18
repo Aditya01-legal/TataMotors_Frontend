b4b1396db1552f4d9e64ee5bab2f06abce361e9d
Aditya01-legal <adityaaditi7930@example.com>
2025-07-18 11:57:38 +0530

    Fix dark mode styles

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import DefectForm from "./components/DefectForm";
 function App() {
   return (
     <div className="min-h-screen bg-gray-950 text-white p-4">
-      <h1 className="text-3xl font-bold mb-4">Daily Issue Tracking</h1>
+      {/* //<h1 className="text-3xl font-bold mb-4">Daily Issue Tracking</h1> */}
       <DefectForm />
     </div>
   );
